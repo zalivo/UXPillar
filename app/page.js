4f7bef7db186641a730f94d4e8aa7def9400d61e
Ivo <izaloudek5@gmail.com>
2024-03-19 17:06:30 +0100

    first commit

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col gap-5 h-dvh bg-[#FEF5EA] justify-center items-center">
+      Gathering knowledge in the fields of UX, UI and HCI 📖
+      <a>Add a resource (will add soon)</a>
+    </div>
+  );
+}
